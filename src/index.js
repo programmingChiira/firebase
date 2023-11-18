ccdafc0f66d55fcfe6176a16b888919547222b8f
programmingChiira <programmingchiira@gmail.com>
2023-11-18 12:57:17 +0300

    deleting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import {initializeApp} from 'firebase/app'
-import{
-    getFirestore, collection, getDocs
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, getDocs,
+    addDoc, deleteDoc, doc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -10,22 +11,47 @@ const firebaseConfig = {
     storageBucket: "fir-project-81767.appspot.com",
     messagingSenderId: "842070026528",
     appId: "1:842070026528:web:d7674a26b48c0a9ae7cf45"
-  };
+};
 
-  initializeApp(firebaseConfig)
+initializeApp(firebaseConfig)
 
-  const db = getFirestore()
+const db = getFirestore()
 
-  const colRef = collection(db, 'books')
+const colRef = collection(db, 'books')
 
-  getDocs(colRef)
+getDocs(colRef)
     .then((snapshot) => {
         let books = []
         snapshot.docs.forEach((doc) => {
-            books.push({ ...doc.data(), id: doc.id})
+            books.push({ ...doc.data(), id: doc.id })
         })
         console.log(books)
     })
     .catch(err => {
         console.log(err.message)
     })
+
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        title: addBookForm.title.value,
+        author: addBookForm.author.value,
+    })
+    .then(() => {
+        addBookForm.reset()
+    })
+})
+
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+    deleteDoc(docRef)
+        .then(() => {
+            deleteBookForm.reset()
+        })
+})
