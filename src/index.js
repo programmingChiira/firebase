8666c9a9bd6ccd0f835df747f321190eedcbd61d
programmingChiira <programmingchiira@gmail.com>
2023-11-19 09:32:49 +0300

    updating documents

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {
     addDoc, deleteDoc, doc,
     query, where,
     orderBy, serverTimestamp,
-    getDoc
+    getDoc, updateDoc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -66,4 +66,19 @@ getDoc(docRef)
 
 onSnapshot(docRef, (doc) => {
     console.log(doc.data(), doc.id)
+})
+
+const updateForm = document.querySelector('.update')
+updateForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', updateForm.id.value)
+
+    updateDoc(docRef, {
+        title: 'Updated title'
+    })
+    .then(() =>{
+        updateForm.reset()
+    })
+    
 })
